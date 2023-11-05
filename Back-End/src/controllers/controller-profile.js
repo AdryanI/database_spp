1b86193ea0ea6e42d8400bfb41de5661c491ba98
erlangftsiesta12 <erlangftsiesta12@gmail.com>
2023-11-05 21:19:28 +0700

    mengupdate query sql, mengupdate controller-profile.js

--- Back-End/src/controllers/controller-profile.js
+++ Back-End/src/controllers/controller-profile.js
@@ -7,9 +7,9 @@ pool.on('error',(err)=> {
     console.error(err);
 });
 
-module.exports ={
-    profile(req,res){
-        let id = req.session.userid
+module.exports = {
+    profile(req, res) {
+        let id = req.session.userid;
         pool.getConnection(function(err, connection) {
             if (err) throw err;
             connection.query(
@@ -17,21 +17,34 @@ module.exports ={
                 SELECT * FROM login where user_id = '${id}';
                 `
             , function (error, results) {
-                if(error) throw error;
-                res.render("profile",{
-                    url: 'http://localhost:5000/',
-                    userName: req.session.username,
-                    nama: results[0]['user_name'],
-                    email: results[0]['user_email'],
-                    status: results[0]['status'],
-                    periode: results[0]['periode'],
-                    foto: results[0]['foto'],
+                if (error) throw error;
+
+                // Sekarang, tambahkan query untuk mengambil data "kompetensi_keahlian" dan "nama_kelas" dari tabel "kelas"
+                connection.query(
+                    `
+                    SELECT * FROM kelas;
+                    `
+                , function (error, kelasResults) {
+                    if (error) throw error;
+
+                    // Selanjutnya, ubah objek data yang akan dikirim ke tampilan
+                    res.render("profile", {
+                        url: 'http://localhost:5000/',
+                        userName: req.session.username,
+                        nama: results[0]['user_name'],
+                        email: results[0]['user_email'],
+                        status: results[0]['status'],
+                        periode: results[0]['periode'],
+                        foto: results[0]['foto'],
+                        kompetensi_keahlian: results[0]['kompetensi_keahlian'],
+                        nama_kelas: results[0]['nama_kelas'],
+                    });
                 });
             });
             connection.release();
         })
     },
-    profileUpload(req, res) {
+ profileUpload(req, res) {
         
         pool.getConnection(function(err, connection) {
             if (err) throw err;
--- db_try.sql
+++ db_try.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2023 at 02:03 PM
+-- Generation Time: Nov 05, 2023 at 03:17 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,11 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(16) NOT NULL,
+  `nama_kelas` varchar(10) NOT NULL,
+  `kompetensi_keahlian` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `kompetensi_keahlian`) VALUES
+(1, 'XI RPL 3', 'Rekayasa Perangkat Lunak');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login`
 --
 
 CREATE TABLE `login` (
   `user_id` int(16) NOT NULL,
+  `photo_id` int(16) NOT NULL,
   `user_name` varchar(64) NOT NULL,
   `user_email` varchar(64) NOT NULL,
   `user_password` text NOT NULL,
@@ -40,14 +60,78 @@ CREATE TABLE `login` (
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`user_id`, `user_name`, `user_email`, `user_password`, `status`, `periode`) VALUES
-(1, 'Erlang', 'erlang@siesta.com', '123', 'admin', '2022-2024'),
-(2, 'Adryan', 'adryan@elaina.com', '123', 'petugas', '2022-2024'),
-(3, 'Rangga', 'rangga@komi.com', '123', 'siswa', '2022-2024');
+INSERT INTO `login` (`user_id`, `photo_id`, `user_name`, `user_email`, `user_password`, `status`, `periode`) VALUES
+(1, 1, 'Erlang', 'erlang@siesta.com', '123', 'admin', '2022-2024'),
+(2, 2, 'Adryan', 'adryan@elaina.com', '123', 'petugas', '2022-2024'),
+(3, 3, 'Rangga', 'rangga@komi.com', '123', 'siswa', '2022-2024'),
+(4, 4, 'Raffi', 'raffi@mai.com', '123', 'siswa', '2022-2024'),
+(5, 5, 'Ronald', 'ronald@kokomi.com', '123', 'siswa', '2022-2024');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `photo_id` int(16) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nisn` int(10) NOT NULL,
+  `nis` char(8) NOT NULL,
+  `nama` varchar(32) NOT NULL,
+  `id_kelas` int(8) NOT NULL,
+  `alamat` text NOT NULL,
+  `nomor_telp` varchar(13) NOT NULL,
+  `id_spp` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`nisn`, `nis`, `nama`, `id_kelas`, `alamat`, `nomor_telp`, `id_spp`) VALUES
+(204001, '1234', 'Rangga Athaya Dzikri', 1, 'Jalan Jalan ', '082116660825', 2040001);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spp`
+--
+
+CREATE TABLE `spp` (
+  `id_spp` int(32) NOT NULL,
+  `user_name` varchar(64) NOT NULL,
+  `user_email` varchar(64) NOT NULL,
+  `perihal` varchar(64) NOT NULL,
+  `nominal` varchar(16) NOT NULL,
+  `waktu` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `spp`
+--
+
+INSERT INTO `spp` (`id_spp`, `user_name`, `user_email`, `perihal`, `nominal`, `waktu`) VALUES
+(2040001, 'Rangga', 'rangga@komi.com', 'SPP November 2023', '350,000', '2023-11-05 08:37:49');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
 
 --
 -- Indexes for table `login`
@@ -56,14 +140,69 @@ ALTER TABLE `login`
   ADD PRIMARY KEY (`user_id`);
 
 --
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`photo_id`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nisn`),
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_spp` (`id_spp`);
+
+--
+-- Indexes for table `spp`
+--
+ALTER TABLE `spp`
+  ADD PRIMARY KEY (`id_spp`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `user_id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `photo_id` int(16) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `nisn` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=204002;
+
+--
+-- AUTO_INCREMENT for table `spp`
+--
+ALTER TABLE `spp`
+  MODIFY `id_spp` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2040002;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_spp`) REFERENCES `spp` (`id_spp`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
